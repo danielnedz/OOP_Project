4e039f589c78d48d14b5fed93f3497490915ee14
danielnedz <danielned113@gmail.com>
2026-05-09 22:23:48 +0300

    Create helpCommand.java

---
+++ helpCommand.java
@@ -0,0 +1,22 @@
+package bg.tu_varna.sit.f24621853.project11.command;
+
+public class HelpCommand implements Command {
+    @Override
+    public void execute(String[] args) {
+        System.out.println("Available commands:");
+        System.out.println("open <file>");
+        System.out.println("close");
+        System.out.println("save");
+        System.out.println("saveas <file>");
+        System.out.println("help");
+        System.out.println("exit");
+        System.out.println("addevent <date> <hall> <name>");
+        System.out.println("freeseats <date> <name>");
+        System.out.println("book <row> <seat> <date> <name> <note>");
+        System.out.println("unbook <row> <seat> <date> <name>");
+        System.out.println("buy <row> <seat> <date> <name>");
+        System.out.println("bookings [<date>] [<name>]");
+        System.out.println("check <code>");
+        System.out.println("report <from> <to> [<hall>]");
+    }
+}
