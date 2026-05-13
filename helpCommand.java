e8c4f9ff0e46cb6276d1508a21159a796e5ace4f
danielnedz <danielned113@gmail.com>
2026-05-13 21:06:45 +0300

    Update helpCommand.java

--- helpCommand.java
+++ helpCommand.java
@@ -1,6 +1,15 @@
 package bg.tu_varna.sit.f24621853.project11.command;
 
+/**
+ * Команда, която извежда всички налични команди в приложението.
+ */
 public class HelpCommand implements Command {
+
+    /**
+     * Извежда информация за всички поддържани команди.
+     *
+     * @param args аргументи на командата, които тук не се използват
+     */
     @Override
     public void execute(String[] args) {
         System.out.println("Available commands:");
